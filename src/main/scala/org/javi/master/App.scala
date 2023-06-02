327bc76ce08ba5dce9cf074b685822f2d49c8a5c
javi <javiruizar@gmail.com>
2023-06-02 02:53:30 +0200

    first commit. Create simple app, read csv and write parquet files

---
+++ src/main/scala/org/javi/master/App.scala
@@ -0,0 +1,44 @@
+package org.javi.master
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
+
+object App {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("Simple Application")
+      .getOrCreate()
+
+    val inputRelativePath = "data/csv/test_data.csv"
+    val inputAbsolutePath = "/"+inputRelativePath
+    val sparkMaster = spark.conf.get("spark.master")
+    val inputPath = sparkMaster match {
+      case "local[*]" => inputRelativePath
+      case _ => inputAbsolutePath
+    }
+    val df = spark.read.option("header", "true").csv(inputPath)
+
+
+    println(df.count())
+    println(spark.conf.get("spark.master"))
+
+    Thread.sleep(3000)
+    df.show(200, false)
+
+    val left = df.select(col("werks"), col("matnr"), col("lgort"))
+    val right = df.select("werks", "matnr", "zcat")
+
+    val finalDf = left.join(right, Seq("werks", "matnr"))
+    finalDf.show(20, false)
+    val outputRelativePath = "data/parquet/"
+    val outputAbsolutePath = "/"+outputRelativePath
+    val outputPath = sparkMaster match {
+      case "local[*]" => outputRelativePath
+      case _ => outputAbsolutePath
+    }
+    df.write.mode("append").parquet(outputPath)
+
+  }
+}
