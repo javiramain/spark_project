26260978093f7a009640d6632ed595448e9292e9
javi <javiruizar@gmail.com>
2023-06-24 18:46:18 +0200

    fix kafka and spark version. writeStream from kafka. Write to kafka after filtering mongo.

--- streaming/src/main/scala/org/javi/master/streaming/StreamingApp.scala
+++ streaming/src/main/scala/org/javi/master/streaming/StreamingApp.scala
@@ -5,27 +5,35 @@ package org.javi.master.streaming
 //import org.apache.spark.SparkConf
 import org.apache.log4j.PropertyConfigurator
 import com.google.crypto.tink.proto.KmsAeadKeyFormat
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.functions.col
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.{Dataset, ForeachWriter, Row}
+import org.apache.spark.sql.functions.{col, lit}
+import org.apache.spark.sql.streaming.StreamingQuery
+import org.apache.spark.sql.types.{StringType, StructType}
 import org.slf4j.LoggerFactory
+
+import java.util.Properties
 //import org.apache.spark.internal.Logging
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 
-object StreamingApp {
+object StreamingApp extends Logging{
 
 
   def main(args: Array[String]): Unit = {
-    val log4jConfPath = "src/main/resources/log4j2.properties"
-    PropertyConfigurator.configure(log4jConfPath)
+//    val log4jConfPath = "src/main/resources/log4j2.properties"
+//    PropertyConfigurator.configure(log4jConfPath)
     val log = LoggerFactory.getLogger(getClass)
 //    conf.set("spark.jars.packages","org.mongodb.spark:mongo-spark-connector:10.0.5,org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1")
-    try {
+//    try {
 
       val sparkConf = new SparkConf()
-        .set("spark.driver.extraJavaOptions", s"-Dlog4j.configuration=file:$log4jConfPath")
-        .set("spark.executor.extraJavaOptions", s"-Dlog4j.configuration=file:$log4jConfPath")
+//        .set("spark.driver.extraJavaOptions", s"-Dlog4j.configuration=file:$log4jConfPath")
+//        .set("spark.executor.extraJavaOptions", s"-Dlog4j.configuration=file:$log4jConfPath")
         .set("spark.mongodb.read.connection.uri", "mongodb://localhost:27017/localDb.Usuarios")
+        .set("spark.sql.streaming.checkpointLocation", "/tmp")
 
       log.info("starting Spark session")
       val ssc = SparkSession
@@ -41,29 +49,7 @@ object StreamingApp {
         case "local[*]" => "localhost:9095"
         case _ => "workernode1:9092,workernode2:9093,workernode3:9094"
       }
-      val KafkaStream = ssc
-        .read
-        .format("kafka")
-        .option("kafka.bootstrap.servers", bootstrapServer)
-        .option("subscribe", "streaming-query")
-        .option("startingOffsets", "earliest")
-        .load()
-        .selectExpr("CAST(value AS STRING) as BUSQUEDA")
-      // cuando pones el writeStream tienes que ponerle el start.
-
-      val a = KafkaStream.select("BUSQUEDA").map(r => r.getString(0)).collect.flatten.mkString
-      println(a)
-      val mongoDataBase = ssc.read
-//        .format("com.mongodb.spark.sql.DefaultSource")
-        .format("mongodb")
-        .load()
-
-
-      println("consulta")
-      val mongoInfo = mongoDataBase.select(col("nombre"))
-        .filter(col("apellido").contains(a))
-//        .show(false)
-      val user = "user"
+      /*
 
       mongoInfo
         .write
@@ -79,14 +65,98 @@ object StreamingApp {
       //      .format("console").outputMode("append")
 //        .start().awaitTermination()
 
+*/
+      val mongoDataBase = ssc.read
+        .format("mongodb")
+        .load()
 
-    }
-    catch {
-      case e: Exception =>
-        println(e)
-        log.error(s"Ocurrió una excepción de tipo ${e.getClass.getSimpleName} durante la ejecución:")
-        log.error(e.getMessage)
-    }
+      // Leer los mensajes desde el topic de Kafka
+      val kafkaDF = ssc.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers",bootstrapServer)
+        .option("subscribe", "streaming-query")
+        .option("startingOffsets", "earliest")
+        .load()
+        .selectExpr("CAST(value AS STRING) as BUSQUEDA")
+
+//    kafkaDF.toJSON.writeStream
+//      .format("kafka")
+//      .option("kafka.bootstrap.servers",bootstrapServer)
+//      .option("topic", "output")
+//      .start().awaitTermination()
+
+//
+
+//
+//      val a = kafkaDF.select("BUSQUEDA").writeStream.outputMode("memory").start()
+//
+//      val mongoInfo = mongoDataBase
+//        .select(col("nombre"))
+//        .as[String]
+
+
+//    val output = mongoDataBase
+    //            .as[String]
+//    output.show(false)
+//    output
+//      .write
+//      .format("kafka")
+//      .option("kafka.bootstrap.servers", "localhost:9095")
+//      .option("topic", "output")
+//      .save()
+
+      kafkaDF
+        .writeStream
+        .foreachBatch { (batchDF: Dataset[Row], batchId: Long) =>
+//          val filtro = batchDF.select("BUSQUEDA")
+          batchDF.show(false)
+          //meterle un try por si cuando se ejecute no se ha recibido ningun mensaje aun
+          val a = batchDF.select("BUSQUEDA").collect()(0).mkString
+          val output = mongoDataBase.filter(col("apellido")===a).select("nombre")
+            .withColumn("value", col("nombre"))
+//            .as[String]
+          output.show(false)
+          output
+            .write
+            .format("kafka")
+            .option("kafka.bootstrap.servers", "localhost:9095")
+            .option("topic", "output")
+//            .format("parquet").mode("append")
+//            .save("/home/javi/Documentos/TFM/spark_project/streaming/salida/")
+            .save
+
+          println("saved results")
+          ()
+        }
+          .start().awaitTermination()
+
+//      kafkaDF.writeStream.start()
+
+      //
+//
+//
+//      val query = mongoInfo
+//        .writeStream
+//        .format("kafka")
+//        .option("kafka.bootstrap.servers", "localhost:9095")
+//        .option("topic", "output")
+//        .start()
+////        .foreachBatch { (batchDF, batchId) =>
+////          batchDF.persist() // Persistir el DataFrame para optimizar el rendimiento en caso de múltiples operaciones
+////          val resultados = batchDF.collect() // Obtener los resultados del batchDF
+////          resultados.foreach(resultado => writer.process(resultado)) // Enviar cada resultado a Kafka
+////          batchDF.unpersist() // Liberar la persistencia del DataFrame
+////        }
+//      // Iniciar la consulta
+//      query.awaitTermination()
+//
+
+//    }
+//    catch {
+//      case e: Exception =>
+//        println(e)
+//        log.error(s"Ocurrió una excepción de tipo ${e.getClass.getSimpleName} durante la ejecución:")
+//        log.error(e.getMessage)
+//    }
   }
 }
-
